452f472a375d619e966fbbd980cc164b07bf6f3d
sungwoong.park <ritchiepark6@gmail.com>
2022-12-02 15:36:48 +0900

    Change of data type

--- build_db.sql
+++ build_db.sql
@@ -4,7 +4,7 @@ USE diary;
 
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(256),
+    user_id VARCHAR(256) UNIQUE,
     password VARCHAR(256),
     user_name VARCHAR(256),
     user_email VARCHAR(256),
@@ -19,16 +19,14 @@ VALUES ("abc123", "4119f341f23c55ebf02d7bd735946d6a8cf03f390ff6a9c7c1ce6641a5469
 
 -- ID: abc123 , pwd: 112980317 password is crypted in SHA256 in crypto-js SHA256
 
-
 CREATE TABLE IF NOT EXISTS logday(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(256),
-    date DATE,
-    question_set VARCHAR(256)
+    user_id VARCHAR(256) UNIQUE,
+    question_set JSON
 );
 
 INSERT INTO logday(user_id, date, question_set)
-VALUES("abc123", "2022-11-18", "Number of pushups|");
+VALUES("abc123", '["You good?", "How many times did you push up?", "Did you have a good day?", "Decribe your day"]');
 
 -- today's date can be obtained by DATE_FORMAT(now(), '%Y-%m-%d')
 -- question set is question strings that added all of the questions
@@ -36,15 +34,26 @@ VALUES("abc123", "2022-11-18", "Number of pushups|");
 CREATE TABLE IF NOT EXISTS questions(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(256),
-    date DATE,
-    question_order INT,
     question VARCHAR(256),
-    question_value VARCHAR(256),
-    question_type VARCHAR(256)
+    qeustion_selection JSON,
+    question_type VARCHAR(256),
+    question_answers JSON
 );
 
-INSERT INTO questions(user_id, date, question_order, question, question_value, question_type) VALUES(
-    "abc123", "2022-11-18", 0, "Number of pushups", "20", "Number"
+INSERT INTO questions(user_id, date, question, qeustion_selection, question_type, question_answers) VALUES(
+    "abc123", "You good?", "[Good, Soso, Bad]", "multiple choice", '[{"date": "2022-12-1", "answer": "Good"}, {"date": "2022-12-1", "answer": "Bad"}]'
+);
+
+INSERT INTO questions(user_id, date, question, qeustion_selection, question_type, question_answers) VALUES(
+    "abc123", "How many times did you push up?", "", "number", '[{"date": "2022-12-1", "answer": "10"}, {"date": "2022-12-1", "answer": "40"}]'
+);
+
+INSERT INTO questions(user_id, date, question, qeustion_selection, question_type, question_answers) VALUES(
+    "abc123", "Did you have a good day?", "", "bolean", '[{"date": "2022-12-1", "answer": "TRUE"}, {"date": "2022-12-1", "answer": "FALSE"}]'
+);
+
+INSERT INTO questions(user_id, date, question, qeustion_selection, question_type, question_answers) VALUES(
+    "abc123", "Decribe your day", "", "text", '[{"date": "2022-12-1", "answer": "It was good"}, {"date": "2022-12-1", "answer": "It was bad"}]'
 );
 
 -- compare with date and user_id and get question and questoion_value
